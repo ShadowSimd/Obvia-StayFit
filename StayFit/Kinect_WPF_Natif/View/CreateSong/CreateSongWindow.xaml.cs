c07187e3a30f7c083810d6d5d603ac006df3ae28
Simon Dery <simondery21@gmail.com>
2026-03-04 21:16:04 -0500

    Création des components frontend pour le song creator, Load les moves et Load les chansons

--- StayFit/Kinect_WPF_Natif/View/CreateSong/CreateSongWindow.xaml.cs
+++ StayFit/Kinect_WPF_Natif/View/CreateSong/CreateSongWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Kinect_WPF_Natif.Model;
+using Kinect_WPF_Natif.Model.DTO;
+using Kinect_WPF_Natif.Model.ML;
+using Kinect_WPF_Natif.Model.Play;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static Kinect_WPF_Natif.Model.ML.MoveHandler;
 
 namespace Kinect_WPF_Natif.View.CreateSong
 {
@@ -19,9 +24,61 @@ namespace Kinect_WPF_Natif.View.CreateSong
     /// </summary>
     public partial class CreateSongWindow : Window
     {
+        private SongSelectItem _currentSong = null;
+
         public CreateSongWindow()
         {
             InitializeComponent();
+            InitializeMoves();
+            InitializeSongSelector();
+        }
+
+        /// <summary>
+        ///     Simon Déry - 4 mars 2026
+        ///     Permet de créer des boutons contenant les moves diposnibles de l'IA
+        /// </summary>
+        private void InitializeMoves()
+        {
+            List<Move> availableMoves = MoveHandler.GetAllMoves()
+                .Where(m => m.MoveId != Moves.None).ToList();
+
+            MovesControl.ItemsSource = availableMoves;
+        }
+
+        /// <summary>
+        ///     Simon Déry - 4 mars 2026
+        ///     Initialise le song selector
+        /// </summary>
+        private void InitializeSongSelector()
+        {
+            List<SongSelectItem> songs = Constants.AVAILABLE_SONGS;
+            SongSelector.ItemsSource = songs;
+            SongSelector.SelectedIndex = 0;
+        }
+
+        public void MoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = sender as Button;
+
+            if (clickedButton?.DataContext is Move selectedMove)
+            {
+                lblConsole.Content = $"MoveButton_Click: Clicked on {selectedMove.DisplayName}";
+            }
+            else
+                lblConsole.Content = "MoveButton_Click : Not Found";
+        }
+
+        private void btnCreer_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentSong != null)
+            {
+                lblConsole.Content = "btnCreer_Click : A song is already playing";
+                return;
+            }
+
+            _currentSong = SongSelector.SelectedItem as SongSelectItem;
+            lblConsole.Content = $"btnCreer_Click : Playing song: {_currentSong.Title}";
+
         }
     }
 }
